2c063b77db85f7e96a39bcb5b42367463e0be288
Alpenbelieve <906062523@qq.com>
2021-06-19 17:28:31 +0800

    添加removeAllElements方法，添加泛型支持，修复少量bug

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,7 @@
-public class Array {
-    private int[] data;
+import java.util.ArrayList;
+
+public class Array<E> {
+    private E[] data;
     private int size;
 
     public Array() {
@@ -7,7 +9,7 @@ public class Array {
     }
 
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -16,7 +18,7 @@ public class Array {
         StringBuilder builder = new StringBuilder(String.format("Array: size=%d, capacity=%d\n[", size, data.length));
         for (int i = 0; i < size; i++) {
             builder.append(data[i]);
-            if (i!=size-1){
+            if (i != size - 1) {
                 builder.append(", ");
             }
         }
@@ -31,109 +33,145 @@ public class Array {
         return data.length;
     }
 
-    public int get(int index){
-        if (index<0 || index>=size){
+    public E get(int index) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException(String.format("Get failed. Index %d is invalid.)", index));
         }
         return data[index];
     }
 
-    public void set(int index, int element){
-        if (index<0 || index>=size){
+    public void set(int index, E element) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException(String.format("Get failed. Index %d is invalid.)", index));
         }
         data[index] = element;
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public void add(int element, int index){
-        if (size==data.length){
+    public void add(E element, int index) {
+        if (size == data.length) {
             throw new IllegalArgumentException("Add element failed. Array is full.");
         }
-        if (index<0 || index>size){
+        if (index < 0 || index > size) {
             throw new IllegalArgumentException(String.format("Add failed. Index %d is invalid.)", index));
         }
         for (int i = size; i > index; i--) {
-            data[i] = data[i-1];
+            data[i] = data[i - 1];
         }
         data[index] = element;
         size++;
     }
 
-    public void addLast(int element){
+    public void addLast(E element) {
         add(element, size);
     }
 
-    public void addFirst(int element){
+    public void addFirst(E element) {
         add(element, 0);
     }
 
-    public int find(int element){
-        for (int i = 0; i < data.length; i++) {
-            if (data[i]==element)
+    public int find(E element) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(element))//不能用==
                 return i;
         }
         return -1;
     }
 
-    public boolean contains(int element){
-        for (int i = 0; i < data.length; i++) {
-            if (data[i]==element)
+    public ArrayList<Integer> findAll(int element) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(element))//不能用==
+                result.add(i);
+        }
+        return result;
+    }
+
+    public boolean contains(int element) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(element))//不能用==
                 return false;
         }
         return false;
     }
 
-    public boolean removeElement(int element){
+    //只会删除第一个
+    public boolean removeFirstElement(E element) {
         int index = find(element);
-        if (index==-1)
+        if (index == -1)
             return false;
         for (int i = index; i < size; i++) {
-            data[i] = data[i+1];
+            data[i] = data[i + 1];
         }
         size--;
         return true;
     }
 
-    public int remove(int index){
-        if (index<0 || index>=size){
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException(String.format("Remove failed. Index %d is invalid.", index));
         }
-        int result = data[index];
-        for (int i = index; i < size-1; i++) {
-            data[i] = data[i+1];
+        E result = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
         }
         size--;
+        data[size] = null;//这一句也可以不用
         return result;
     }
 
-    public int removeFirst(){
+    public boolean removeAllElements(int element) {
+        ArrayList<Integer> indexes = findAll(element);
+        if (indexes.size() == 0)
+            return false;
+        int index = 0;
+        int i = 0, j = 0;
+        while (j < size) {
+            if (j == indexes.get(index)) {
+                j++;
+                index++;
+                if (index == indexes.size()) {
+                    while (j < size)
+                        data[i++] = data[j++];
+                    for (int k = j - 1; k >= i; k--)
+                        data[k] = null;
+                    size = i;
+                    return true;
+                }
+            } else
+                data[i++] = data[j++];
+        }
+        for (int k = j - 1; k >= i; k--)
+            data[k] = null;
+        size = i;
+        return true;
+    }
+
+    public E removeFirst() {
         return remove(0);
     }
 
-    public int removeLast(){
-        return remove(size-1);
+    public E removeLast() {
+        return remove(size - 1);
     }
 
     public static void main(String[] args) {
-        Array array = new Array(20);
+        Array<Integer> array = new Array<>(20);
         System.out.println(array);
         for (int i = 0; i < 10; i++) {
-            array.addLast(i);
+            array.addLast(10);
         }
         System.out.println(array);
-        array.add(100,1);
+        array.add(100, 3);
         System.out.println(array);
-        array.addFirst(-1);
+        array.addLast(100);
         System.out.println(array);
-        array.remove(2);
+        array.addFirst(100);
         System.out.println(array);
-        array.removeElement(4);
-        System.out.println(array);
-        array.removeFirst();
+        System.out.println(array.removeAllElements(10));
         System.out.println(array);
     }
 }
